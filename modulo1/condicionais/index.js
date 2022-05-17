ddeb12e05b7dd64b5c28e1e47b5ae0bf1af5b918
raphacandea <raphaelcandea@hotmail.com>
2022-05-17 13:50:11 -0300

    exercicios condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,71 @@
+// Exercícios de interpretação escrita
+// 1) A) O teste verifuca se o número testado é par
+// B) Números pares
+// C) Números ímpares
+
+//2) 
+
+// A) Para saber o preço das frutas
+// B) O preço da fruta, maçã é R$ 2.25
+// C) 5
+
+// 3)
+// A) Pedindo para um usuário digitar um número no console
+// B) Quando digita 10 Esse número passou no teste e se fosse -10 não apareceria nada
+// C) Sim, ele está puxando uma função que não está definida
+
+// Exercícios de escrita de código
+// 1)
+// let idadeUsuario = +prompt(`Qual a sua idade?`)
+
+// if (idadeUsuario >= 18) {
+//     console.log(`Você pode dirigir`)
+// } else {
+//     console.log(`Você não pode dirigir`)
+// }
+
+// 2) 
+// let turnoUsuario = prompt('Qual turno você estuda? Digite M para matutino, V para vespertino e N para noturno')
+
+// if (turnoUsuario === 'M') {
+//     console.log('Bom dia')
+// } else if (turnoUsuario === 'V') {
+//     console.log('Boa tarde')
+// } else {
+//     console.log('Boa noite')
+// }
+
+// 3)
+
+// let turnoUsuario = prompt('Qual turno você estuda? Digite M para matutino, V para vespertino e N para noturno')
+
+// switch (turnoUsuario) {
+//     case 'M':
+//         console.log('Bom dia')
+        
+//         break;
+//     case 'V':
+//         console.log('Boa tarde')
+    
+//         break;
+
+//     case 'N':
+//         console.log('Boa noite')
+
+//         break
+
+
+//     default:
+//         break;
+// }
+
+// // 4)
+
+// generoDoFilme = prompt('Qual gênero do filme vamos assistir?')
+// precoIngresso = prompt('Qual valor do ingresso?')
+
+// if (generoDoFilme === 'Fantasia' && precoIngresso < 15) {
+//     console.log('Bom filme')
+// } else {
+//     console.log('Escolha outro filme')
+// }
